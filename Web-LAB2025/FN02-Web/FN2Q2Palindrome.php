64ff0e78b1813b413e11ee8249304dfb8713baf3
Abhin M <abhinm24@gmail.com>
2025-01-30 21:03:58 +0530

    Updates

--- Web-LAB2025/FN02-Web/FN2Q2Palindrome.php
+++ Web-LAB2025/FN02-Web/FN2Q2Palindrome.php
@@ -6,17 +6,21 @@
     <title>Palindrome</title>
 </head>
 <body>
+
     <form method="post">
         Input <input type="text" name="Palin" id="" value="<?=(isset($_POST['Palin']))?$_POST['Palin']:""?>">
         <button type='submit'>Check</button>
     </form>
+
     <?php
+    
         function palindrome($Value){
             if (str_replace(" ","",$Value)==""){
                 return false;
             }
             return ($Value==strrev($Value));
         }
+
         if(isset($_POST['Palin'])){
             if(palindrome($_POST['Palin'])){
                 echo $_POST['Palin']." is a Palindrome ";
