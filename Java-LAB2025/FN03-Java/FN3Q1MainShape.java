5c899e8cfe71783d7e48cb5a520bef25cc61ff8e
Abhin M <abhinm24@gmail.com>
2025-02-01 18:03:32 +0530

    Updates

---
+++ Java-LAB2025/FN03-Java/FN3Q1MainShape.java
@@ -0,0 +1,30 @@
+class Shape{
+
+    int x,y;
+    public void getxyvalue(int x, int y){
+        this.x=x;
+        this.y=y;
+    }
+    public void showxyvalue(){
+        System.out.println("X: "+x+"  Y: "+y);
+    }
+}
+
+class Rectangle  extends Shape{
+
+    int x,y;
+    Rectangle(int x, int y){
+        this.x=x;
+        this.y=y;
+    }
+    public void showxyvalue(){
+        System.out.println("X: "+x+"  Y: "+y);
+    }
+}
+
+class FN3Q1MainShape{
+    public static void main(String ar[]){
+        Rectangle rect= new Rectangle(20,30);
+        rect.showxyvalue();
+    }
+}
