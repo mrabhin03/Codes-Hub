f087676d49dcf13dae9110be24f0661ba1770e3f
Abhin M <abhinm24@gmail.com>
2025-02-02 18:35:49 +0530

    Update

--- Java-LAB2025/AN01-Java/AN1Q1FileManage.java
+++ Java-LAB2025/AN01-Java/AN1Q1FileManage.java
@@ -3,11 +3,12 @@ import java.util.Scanner;
 public class AN1Q1FileManage {
     public static void main(String ar[]){
         try{
+
             Scanner read=new Scanner(System.in);
             System.out.print("Enter the File name to Create: ");
             String fileName=read.nextLine();
             File Thefile=new File(fileName);
-            if(Thefile.createNewFile()){
+            if(Thefile.createNewFile()){ // Creating File
                 System.out.println("File Created: ");
             }else{
                 System.out.println("File Already exist: ");
@@ -17,21 +18,25 @@ public class AN1Q1FileManage {
             FileWriter fileWriter=new FileWriter(fileName);
             System.out.print("Enter Enter the value to insert to file: ");
             String info=read.nextLine();
-            fileWriter.write(info);
+            fileWriter.write(info); // Writing to file
             fileWriter.close();
 
             Scanner FileValue=new Scanner(Thefile);
             System.out.println("The Data in "+fileName+" : ");
             while(FileValue.hasNextLine()){
-                System.out.println(FileValue.nextLine());
+                System.out.println(FileValue.nextLine()); // Reading from file
             }
             FileValue.close();
 
             System.out.println("Press 1 to delete the file: ");
             int option=read.nextInt();
             if(option==1){
-                System.out.println("File DELETED");
-                Thefile.delete();
+                if(Thefile.delete()){ // Deleting file
+                    System.out.println("File DELETED");
+                }else{
+                    System.out.println("File not DELETED");
+                }
+                
             }
             
 
