153314cbca280a724168bb574678b1d9e9217c6f
Abhin M <abhinm24@gmail.com>
2025-02-08 15:49:01 +0530

    Updates

---
+++ Java-LAB2025/FN08-Java/FN8ComplexNo.java
@@ -0,0 +1,28 @@
+
+public class FN8ComplexNo {
+    public static void main(String ar[]){
+        Complex obj1=new Complex(2.4,5.3);
+        Complex obj2=new Complex(1.6,4.7);
+        Complex result=obj1.addValues(obj2);
+        result.displayValues();
+
+    }
+}
+
+
+class Complex{
+    double real,imaginary;
+    Complex(double real,double imaginary){
+        this.real=real;
+        this.imaginary=imaginary;
+    }
+    void displayValues(){
+        System.out.println("Real Number: "+real);
+        System.out.println("Imaginary Number: "+imaginary);
+    }
+    Complex addValues(Complex obj){
+        double Treal=this.real+obj.real;
+        double Timag=this.imaginary+obj.imaginary;
+        return new Complex(Treal,Timag);
+    }
+}
