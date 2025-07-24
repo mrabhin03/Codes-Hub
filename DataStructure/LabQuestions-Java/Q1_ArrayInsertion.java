7c63b7b3c7d9cf20716735697a37cd06cd624ccd
Abhin M <abhinm24@gmail.com>
2025-07-24 11:53:07 +0530

    DataStructure Questions

---
+++ DataStructure/LabQuestions-Java/Q1_ArrayInsertion.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Q1_ArrayInsertion {
+    public static void main(String ar[]){
+        int n;
+        Scanner read=new Scanner(System.in); 
+        System.out.print("Enter the Array size: ");
+        n=read.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=read.nextInt();
+        }
+        System.out.print("The Array elements are: ");
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+", ");
+        }
+    }
+}
