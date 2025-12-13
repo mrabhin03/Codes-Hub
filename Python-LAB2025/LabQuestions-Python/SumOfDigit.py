350d95b17c3f92074b256509e4c96f8f90e24152
Abhin M <abhinm24@gmail.com>
2025-12-13 10:51:37 +0530

    Minor changes

--- Python-LAB2025/LabQuestions-Python/SumOfDigit.py
+++ Python-LAB2025/LabQuestions-Python/SumOfDigit.py
@@ -1,7 +1,12 @@
-number=input("Enter the numer: ")
+numy=number=input("Enter the numer: ")
 
 sod=0
-for num in number:
-    sod+=int(num)
+while sod==0 or sod>9:
+    print("reduse:",number)
+    sod=0
+    for num in number:
+        sod+=int(num)
+    number=str(sod)
+    
 
-print("The sum of digits of",number,"is:",sod)
+print("The sum of redused digits of",numy,"is:",sod)
